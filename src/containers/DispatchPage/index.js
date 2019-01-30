a218b49fdde6cc5ecae917990f136f9d97665e5c
Kent <kenoboe.luck@gmail.com>
2019-01-30 17:14:29 +0800

    [https://trello.com/c/CHqXUTDI] Dispatch Page add a "End The Exam" button feature: - Popup a confirm dialog before ending exam - Update some wordings - Use localstorage to get user name

--- src/containers/DispatchPage/index.js
+++ src/containers/DispatchPage/index.js
@@ -29,6 +29,7 @@ import ReactPage from './ReactPage';
 import JavaScriptPage from './JavaScriptPage';
 import ControlWidget from './ControlWidget';
 import SnapCommentBar from './SnapCommentBar';
+import User from 'utils/user';
 
 import styles from './DispatchPage.module.scss';
 
@@ -286,10 +287,10 @@ class Page extends Component {
 
   onCreateComment = async data => {
     const { id } = this.props.record;
-    const { author, content } = data.input;
+    const { content } = data.input;
     const params = {
       commentRecordId: id,
-      author,
+      author: User.getUserName(),
       content,
     };
     await createComment(params);
