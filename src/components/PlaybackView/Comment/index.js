3f39e45b68539a8995f1f80f1e06ceeebfee0e15
wan_yi <jwtfmmrr0303@gmail.com>
2019-02-15 15:48:01 +0800

    Fix: fix problems

--- src/components/PlaybackView/Comment/index.js
+++ src/components/PlaybackView/Comment/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { List, Avatar } from 'antd';
 
@@ -7,39 +7,33 @@ import style from './comment.module.scss';
 const ListItem = List.Item;
 const ListItemMeta = List.Item.Meta;
 
-class CommentArea extends PureComponent {
-  render() {
-    const { comments } = this.props;
-    return (
-      <div className={style.commentarea}>
-        <h4 className={style.title}>Comments</h4>
-        <div className={style.comments}>
-          {comments ? (
-            <List
-              itemLayout="horizontal"
-              dataSource={comments}
-              renderItem={item => (
-                <ListItem className={style.listItem}>
-                  <ListItemMeta
-                    className={style.listMeta}
-                    avatar={<Avatar className={style.avatar} icon="user" />}
-                    title={item.author}
-                    description={item.content}
-                  />
-                </ListItem>
-              )}
-            />
-          ) : (
-            <></>
+const CommentArea = ({ comments }) => (
+  <div className={style.commentarea}>
+    <h4 className={style.title}>Comments</h4>
+    <div className={style.comments}>
+      {comments ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={comments}
+          renderItem={item => (
+            <ListItem className={style.listItem}>
+              <ListItemMeta
+                className={style.listMeta}
+                avatar={<Avatar className={style.avatar} icon="user" />}
+                title={item.author}
+                description={item.content}
+              />
+            </ListItem>
           )}
-        </div>
-      </div>
-    );
-  }
-}
+        />
+      ) : (
+        <></>
+      )}
+    </div>
+  </div>
+);
 
 CommentArea.propTypes = {
   comments: PropTypes.array,
 };
-
 export default CommentArea;
