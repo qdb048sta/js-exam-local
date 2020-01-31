80eaaf0717dda067aa8335d09763c8bb61fe0417
may.kuo <meifutoolate@gmail.com>
2020-01-31 15:35:41 +0800

    modify test files

--- src/components/Grid/index.spec.js
+++ src/components/Grid/index.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Grid from '../../../components/Grid';
+import Grid from './index';
 
 describe('Grid index spec', () => {
   const mockLayout = [
