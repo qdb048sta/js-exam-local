1a4c15dbb6cb387c3dad55e6c5f540079e61e811
may.kuo <meifutoolate@gmail.com>
2020-02-03 12:03:38 +0800

    remove enzyme-to-json/serializer in jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src/'],
   moduleFileExtensions: ['js'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/src/__mocks__/fileMock.js',
