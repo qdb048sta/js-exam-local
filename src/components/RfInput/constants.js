ae04e91009a10cad63c270da77446736cf5ecb7b
Wayne Yang <wayne.yang@ISTWLAPAPPLE2053.local>
2020-03-25 12:05:11 +0800

    Summary information button update

--- src/components/RfInput/constants.js
+++ src/components/RfInput/constants.js
@@ -2,9 +2,11 @@ export const rateTips = {
   Quality: 'Technical skill on solving such problem.',
   Hint: ' How many hints were given.',
   Completeness: 'How much did interviewee complete the problem.',
-  Logic: 'Logic',
-  Language: 'Language',
-  Goodtoworkwith: 'Workwith',
+  Logic: 'Candidate has great idea and thoughtful plan to solve a problem',
+  Language:
+    'Candidate is familiar with basic English communication which can be applied on work',
+  Goodtoworkwith:
+    'Candidate has an nice personality to be a greate partner to work with',
 };
 
 export const rateDesc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
